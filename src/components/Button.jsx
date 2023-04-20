c09a61a9b5cfeb7b54690b2dd0bdc3967bea97d9
Puchana <puchana123@gmail.com>
2023-04-20 15:49:37 +0700

    UI-Business

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-xl ${styles}`}
+    >
+      Get Started
+    </button>
+  );
+};
 
-export default Button
+export default Button;
